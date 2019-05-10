aa31ba8ea0ddf8a9e77ace5add87dbb39a950dc3
Gekiryuu86 <jikannotorento@gmail.com>
2019-05-10 11:45:59 -0600

    regenerated with different options

--- wp-login.php
+++ wp-login.php
@@ -9,11 +9,11 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Log In &lsaquo; Gekiryuu86&#039;s Projects and Blog &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' id='dashicons-css'  href='/wordpress-static/wp-includes/css/dashicons.min.css?ver=5.2' type='text/css' media='all' />
-<link rel='stylesheet' id='buttons-css'  href='/wordpress-static/wp-includes/css/buttons.min.css?ver=5.2' type='text/css' media='all' />
-<link rel='stylesheet' id='forms-css'  href='/wordpress-static/wp-admin/css/forms.min.css?ver=5.2' type='text/css' media='all' />
-<link rel='stylesheet' id='l10n-css'  href='/wordpress-static/wp-admin/css/l10n.min.css?ver=5.2' type='text/css' media='all' />
-<link rel='stylesheet' id='login-css'  href='/wordpress-static/wp-admin/css/login.min.css?ver=5.2' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='/wp-includes/css/dashicons.min.css?ver=5.2' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='/wp-includes/css/buttons.min.css?ver=5.2' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='/wp-admin/css/forms.min.css?ver=5.2' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='/wp-admin/css/l10n.min.css?ver=5.2' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='/wp-admin/css/login.min.css?ver=5.2' type='text/css' media='all' />
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
@@ -22,7 +22,7 @@
 		<div id="login">
 		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
 	
-	<form name="loginform" id="loginform" action="/wordpress-static/wp-login.php" method="post">
+	<form name="loginform" id="loginform" action="/wp-login.php" method="post">
 	<p>
 		<label for="user_login">Username or Email Address<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" /></label>
@@ -40,7 +40,7 @@
 	</form>
 
 			<p id="nav">
-					<a href="/wordpress-static/wp-login.php?action=lostpassword">Lost your password?</a>
+					<a href="/wp-login.php?action=lostpassword">Lost your password?</a>
 				</p>
 	
 	<script type="text/javascript">
@@ -57,7 +57,7 @@
 			if(typeof wpOnload=='function')wpOnload();
 			</script>
 
-			<p id="backtoblog"><a href="/wordpress-static/">
+			<p id="backtoblog"><a href="/">
 		&larr; Back to Gekiryuu86&#039;s Projects and Blog	</a></p>
 			
 	</div>
